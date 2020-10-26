5ff83940719a20d4bbe9534ac551c6e0d0fc3694
plambjoe <plambjoe@students.zhaw.ch>
2020-10-26 12:52:16 +0100

    implements Praktikum06

---
+++ Praktikum06/MyRankingVisitor.java
@@ -0,0 +1,13 @@
+public class MyRankingVisitor implements Visitor<Competitor> {
+    private StringBuilder buf;
+    private int rank=1;
+
+    public MyRankingVisitor(StringBuilder buf) {
+        this.buf = buf;
+    }
+
+    public void visit(Competitor o) {
+        o.setRank(rank++);
+        buf.append(o.toString()+'\n');
+    }
+}
---
+++ Praktikum06/RankingTreeServer.java
@@ -0,0 +1,37 @@
+
+public class RankingTreeServer implements CommandExecutor {
+
+    private final static int STARTNR = 0;
+    private final static int NAME = 1;
+    private final static int JG = 2;
+    private final static int COUNTRY = 3;
+    private final static int TIME = 4;
+    Tree<Competitor> tree = new SortedBinaryTree<>();
+    //Tree<Competitor> tree = new ALVSearchTree<>();
+
+    public void load(Tree<Competitor> tree, String list) throws Exception {
+        String[] lines = list.split("\n");
+        for (String line : lines) {
+            String[] items = line.split(";");
+            Competitor c = new Competitor(Integer.parseInt(items[STARTNR]), // startNr
+                    items[NAME], // name
+                    Integer.parseInt(items[JG]), // jg
+                    items[COUNTRY], // country
+                    items[TIME]); // time
+            tree.add(c);
+        }
+    }
+
+    public String execute(String command) throws Exception {
+        // list of all Competitors
+
+        //read values from String
+        load(tree,command);
+        StringBuilder buf = new StringBuilder();
+        Traversal<Competitor> trav = tree.traversal();
+        Visitor<Competitor> vis = new MyRankingVisitor(buf);
+        trav.inorder(vis);
+        buf.append("\nheight:").append(tree.height());
+        return buf.toString();
+    }
+}
